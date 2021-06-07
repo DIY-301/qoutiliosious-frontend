ffd7db09821123dcd4db0c76d626028b7a6550b1
LayanaBaba <leana_baba@yahoo.com>
2021-06-07 20:09:40 +0300

    memes page edit and header css

--- src/Memes.js
+++ src/Memes.js
@@ -4,9 +4,11 @@ import { Form, Button, NavDropdown, Container, Navbar, Nav, Card, CardGroup, ful
 
 let memesArr = ['10-Guy', 'Angry-Baby', 'Business-Cat', 'Disaster-Girl', 'Ace-Primo', 'Advice-Dog', 'Yao-Ming', 'Warning-Sign', 'Waiting-Skeleton', 'Woah-Kitty'];
 
-let memesArr2 = [ 'Why-Is-The-Rum-Gone','Snoop','Smiling-Cat','Shocked-Ape','Shaq-Only-Smokes-The-Dankest','Shouter','Shrek-Cat','Sponegebob-Coffee','Spiderman-Camera','Spangles'];
+let memesArr2 = ['Why-Is-The-Rum-Gone', 'Snoop', 'Smiling-Cat', 'Shocked-Ape', 'Shaq-Only-Smokes-The-Dankest', 'Shouter', 'Shrek-Cat', 'Sponegebob-Coffee', 'Spiderman-Camera', 'Spangles'];
 
-let memesArr3 =['Squidward','Stoner-PhD','Success-Kid-Girl','Success-Kid-Original','So-Much-Drama','Sad-Baby','Roll-Safe-Think-About-It','Scooby-Doo','Scared-Cat','Scary-Harry']
+let memesArr3 = ['Squidward', 'Stoner-PhD', 'Success-Kid-Girl', 'Success-Kid-Original', 'So-Much-Drama', 'Sad-Baby', 'Roll-Safe-Think-About-It', 'Scooby-Doo', 'Scared-Cat', 'Scary-Harry'];
+
+let memesArr4 = ['Persian-Cat-Room-Guardian-Single', 'Right-In-The-Childhood', 'Rick', 'Putin', 'Psy-Horse-Dance', 'Picard-Wtf', 'Pothead-Fry', 'Pissed-Off-Obama', 'Pinky-and-the-Brain', 'Question-Rage-Face']
 class Memes extends React.Component {
 
     constructor(props) {
@@ -68,70 +70,82 @@ class Memes extends React.Component {
 
         return (
             <>
-         <div style={{width:'500px', marginLeft:'590px' ,marginBottom:'20px'}}>
-                <Form onSubmit={this.submitter}>
 
-                    <Form.Group className="mb-3" controlId="abcc">
+                <div style={{ marginLeft: '270px', padding: '20px' }}>
+                    <img style={{ height: '350px', marginBottom: '5px', width: '400px' }} src='https://apimeme.com/meme?meme=Scary-Harry&top=DIY+MEMES&bottom='>
+                    </img>
+                    <img style={{ height: '350px', marginBottom: '5px', width: '400px', marginLeft: '60px' }} src='https://apimeme.com/meme?meme=Peter-Griffin-News&top=&bottom=Project+week+news'>
+                    </img>
+                    <img style={{ height: '350px', marginBottom: '5px', width: '400px', marginLeft: '60px' }} src='https://apimeme.com/meme?meme=Right-In-The-Childhood&top=Yahia&bottom=Feeling'>
+                    </img>
+
+                </div>
+                <Form style={{ width: '650px', marginLeft: '700px', marginBottom: '20px', border: 'solid', padding: '20px' }} onSubmit={this.submitter}>
+
+                    <Form.Group className="mb-4" controlId="abcc">
+
+                        <Form.Label style={{ marginLeft: '150px', fontWeight: 'bold', fontSize: '25px' }}>
+                            MEMES GENERATOR
+</Form.Label>
+                    </Form.Group>
+                    <Form.Group className="mb-4" controlId="abcc">
 
                         <Form.Control onChange={this.getTop} type="text" placeholder="TOP TEXT" />
                     </Form.Group>
-                    <Form.Group className="mb-3" controlId="abc">
+                    <Form.Group className="mb-4" controlId="abc">
 
                         <Form.Control onChange={this.getBotton} type="text" placeholder="BOTTOM TEXT" />
                     </Form.Group>
-                    <Button style={{width:'150px',height:'50px', marginLeft:'160px'}} variant="primary" type="submit">
+                    <Button style={{ width: '150px', height: '50px', marginLeft: '200px' }} variant="primary" type="submit">
                         Submit
   </Button>
+
                 </Form>
 
-         </div>
-
-         <div style={{marginTop:"-220px",padding:'10px',marginLeft:'120px'}}>
-             <img style={{height:'200px',marginBottom:'5px',width:'300px'}} src='https://apimeme.com/meme?meme=Scary-Harry&top=DIY+MEMES&bottom=Yahia+!!!'>
-             </img>
-         </div>
-
-         <div style={{marginTop:"-227px",padding:'10px',marginLeft:'1250px'}}>
-             <img style={{height:'200px',marginBottom:'5px',width:'300px'}} src='https://apimeme.com/meme?meme=Peter-Griffin-News&top=&bottom=Project+week+news'>
-             </img>
-         </div>
-
-         
 
 
-           
+                {<CardGroup style={{ width: '100%', marginBottom: '10px' }}>
+                    {memesArr.map((item, idx) => {
+                        return (
+                            <Card key={idx} value={`${item}`} onClick={() => this.getType({ e: item })} >
+                                <Card.Img variant="top" src={`https://apimeme.com/meme?meme=${item}`} style={{ height: '10rem', padding: "5px" }} />
 
-                    {<CardGroup style={{ width: '100%' ,marginBottom:'10px'}}>
-                        {memesArr.map((item, idx) => {
-                            return (
-                                <Card key={idx} value={`${item}`} onClick={() => this.getType({ e: item })} >
-                                    <Card.Img variant="top" src={`https://apimeme.com/meme?meme=${item}`} style={{ height: '10rem', padding: "5px"}} />
+                            </Card>
+                        )
+                    })}
+                </CardGroup>}
+                {<CardGroup style={{ width: '100%', marginBottom: '10px' }}>
+                    {memesArr2.map((item, idx) => {
+                        return (
+                            <Card key={idx} value={`${item}`} onClick={() => this.getType({ e: item })} >
+                                <Card.Img variant="top" src={`https://apimeme.com/meme?meme=${item}`} style={{ height: '10rem', padding: "5px" }} />
 
-                                </Card>
-                            )
-                        })}
-                    </CardGroup>}
-                    {<CardGroup style={{ width: '100%', marginBottom:'10px' }}>
-                        {memesArr2.map((item, idx) => {
-                            return (
-                                <Card key={idx} value={`${item}`} onClick={() => this.getType({ e: item })} >
-                                    <Card.Img variant="top" src={`https://apimeme.com/meme?meme=${item}`} style={{ height: '10rem', padding: "5px"}} />
+                            </Card>
+                        )
+                    })}
+                </CardGroup>}
 
-                                </Card>
-                            )
-                        })}
-                    </CardGroup>}
+                {<CardGroup style={{ width: '100%', marginBottom: '10px' }}>
+                    {memesArr3.map((item, idx) => {
+                        return (
+                            <Card key={idx} value={`${item}`} onClick={() => this.getType({ e: item })} >
+                                <Card.Img variant="top" src={`https://apimeme.com/meme?meme=${item}`} style={{ height: '10rem', padding: "5px" }} />
 
-                    {<CardGroup style={{ width: '100%', marginBottom:'10px' }}>
-                        {memesArr3.map((item, idx) => {
-                            return (
-                                <Card key={idx} value={`${item}`} onClick={() => this.getType({ e: item })} >
-                                    <Card.Img variant="top" src={`https://apimeme.com/meme?meme=${item}`} style={{ height: '10rem', padding: "5px"}} />
+                            </Card>
+                        )
+                    })}
+                </CardGroup>}
 
-                                </Card>
-                            )
-                        })}
-                    </CardGroup>}
+                {<CardGroup style={{ width: '100%', marginBottom: '10px' }}>
+                    {memesArr4.map((item, idx) => {
+                        return (
+                            <Card key={idx} value={`${item}`} onClick={() => this.getType({ e: item })} >
+                                <Card.Img variant="top" src={`https://apimeme.com/meme?meme=${item}`} style={{ height: '10rem', padding: "5px" }} />
+
+                            </Card>
+                        )
+                    })}
+                </CardGroup>}
 
                 <Modal
                     show={this.state.show}
@@ -139,14 +153,14 @@ class Memes extends React.Component {
                     backdrop="static"
                     keyboard={false}
                 >
-                     
-                    <Modal.Header  closeButton>
+
+                    <Modal.Header closeButton>
 
                     </Modal.Header>
-                       
+
                     <Modal.Body>
                         <center>
-{this.state.memesType}
+                            {this.state.memesType}
 
                         </center>
                         <Card style={{ width: '18rem' }}>
