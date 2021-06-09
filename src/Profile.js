f56274e7c84f1a5c9868a43ec7dcec00c93de9ab
rujeenaal-zyoud <rujeenaalzoud@gmail.com>
2021-06-09 17:59:11 +0300

    mergeMybranch

--- src/Profile.js
+++ src/Profile.js
@@ -11,7 +11,7 @@ import axios from 'axios';
 import AddQouteForm from './AddQouteForm';
 import EditQuote from './EditQuoteModal'
 import EditQuoteModal from './EditQuoteModal';
-import { positions, Provider } from "react-alert";
+// import { positions, Provider } from "react-alert";
 
 class Profile extends React.Component {
   constructor(props) {
